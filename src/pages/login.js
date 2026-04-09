9c6b650114c0fbd9dcafcaf626d32723e0491e63
ConArtist35 <wollfeyfox@gmail.com>
2026-04-09 18:47:19 +0300

    Functional register and login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,46 @@
+import { useState, useEffect } from "react";
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3002/register")
+      .then((res) => res.json()) //convert from json to array
+      .then((data) => setUsers(data));
+  }, []); //empty array at the end so it runs only once when it loads
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //submit form so user can log in
+    let loginSuccessful = false;
+
+    if (email.trim() !== "" && password.trim() !== "") {
+      const validUser = users.find(
+        (element) => email === element.email && password === element.password,
+      );
+
+      if (validUser) {
+        loginSuccessful = true;
+      }
+    }
+
+    if (loginSuccessful) {
+      alert("Login successful");
+    } else {
+      alert("Wrong email or password");
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
       <label>email</label>
-      <input></input>
+      <input onChange={(e) => setEmail(e.target.value)}></input>
       <label>password</label>
-      <input></input>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+      ></input>
       <button>OK</button>
     </form>
   );
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,8 @@
 import "./register.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+//starts json server - json-server --watch src/db/profiles.json --port 3002
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -9,6 +12,8 @@ const RegisterPage = () => {
   const [validEmail, setvalidEmail] = useState(false);
   const [validPass, setvalidPass] = useState(false);
   const [validPassRepeat, setvalidPassRepeat] = useState(false);
+
+  const [formIsVisible, setformIsVisible] = useState(true);
 
   const handleEmail = (e) => {
     const value = e.target.value;
@@ -41,40 +46,74 @@ const RegisterPage = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); //prevent refresh
 
     if (validEmail && validPass && validPassRepeat) {
-      //submit form and pass the input values so user can register
+      const jsonData = { email: email, password: password };
+      submitData(jsonData);
+    }
+  };
+
+  const submitData = async (data) => {
+    const response = await fetch("http://localhost:3002/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    await response.json();
+    if (response.ok) {
+      setformIsVisible(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>email</label>
-      <input onChange={handleEmail} value={email}></input>
-      {!validEmail &&
-        email && ( //if the email is invalid and something was typed in then it shows the invalid text
-          <p style={{ color: "red", marginTop: "-20px", marginBottom: "10px" }}>
-            Invalid email format
-          </p>
-        )}
-      <label>password</label>
-      <input onChange={handlePass} input type="password"></input>
-      {!validPass && password && (
-        <p style={{ color: "red", marginTop: "-20px", marginBottom: "10px" }}>
-          Password should be at least 8 characters long and contain one capital
-          letter and special symbol
-        </p>
+    <div>
+      {formIsVisible ? (
+        <form onSubmit={handleSubmit}>
+          <label>email</label>
+          <input onChange={handleEmail} value={email}></input>
+          {!validEmail &&
+            email && ( //if the email is invalid and something was typed in then it shows the invalid text
+              <p
+                style={{
+                  color: "red",
+                  marginTop: "-20px",
+                  marginBottom: "10px",
+                }}
+              >
+                Invalid email format
+              </p>
+            )}
+          <label>password</label>
+          <input onChange={handlePass} type="password"></input>
+          {!validPass && password && (
+            <p
+              style={{ color: "red", marginTop: "-20px", marginBottom: "10px" }}
+            >
+              Password should be at least 8 characters long and contain one
+              capital letter and special symbol
+            </p>
+          )}
+          <label>Enter your password again</label>
+          <input type="password" onChange={RepeatPass}></input>
+          {!validPassRepeat && passwordRepeat && (
+            <p
+              style={{ color: "red", marginTop: "-20px", marginBottom: "10px" }}
+            >
+              Passwords do not match
+            </p>
+          )}
+          <button>Register</button>
+        </form>
+      ) : (
+        <div id="register_success">
+          <h1>You have registered successfully</h1>
+          <Link to="/login">
+            <button>Go to login page</button>
+          </Link>
+        </div>
       )}
-      <label>Enter your password again</label>
-      <input input type="password" onChange={RepeatPass}></input>
-      {!validPassRepeat && passwordRepeat && (
-        <p style={{ color: "red", marginTop: "-20px", marginBottom: "10px" }}>
-          Passwords do not match
-        </p>
-      )}
-      <button>Register</button>
-    </form>
+    </div>
   );
 };
 export default RegisterPage;
